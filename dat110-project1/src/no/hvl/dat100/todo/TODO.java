da1a92b46ca9e1691c76eeaf83d30a358c8a51fc
Lars Michael Kristensen <lmkr@hvl.no>
2020-01-19 14:52:07 +0100

    Added TODO class implementation

---
+++ dat110-project1/src/no/hvl/dat100/todo/TODO.java
@@ -0,0 +1,19 @@
+package no.hvl.dat100.todo;
+
+public class TODO {
+
+	public static String method() {
+		
+		String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
+		
+		return "Metoden " + methodName + " er ikke implementert";
+	}
+	
+	
+	public static String construtor(String className) {
+				
+	   return "Konstruktøren for klassen " + className + " er ikke implementert";
+		
+	}
+
+}
